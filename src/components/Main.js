be4af045beb0bfc1ded4bf213cbbb91c6898a61f
GeorgyByPC <gogaopel@gmail.com>
2019-04-24 22:05:05 +0300

    start states

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,28 @@ import React from 'react';
 import TodoItem from './TodoItem';
 import todosData from '../todosData';
 
-const data = todosData.map(todo => <TodoItem todoItem={todo} key={todo.id} />)
-
-function Main() {
-  const style = {
-    fontSize : 30,
-    color: "#FF8C00" 
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: todosData,
+    }
   }
-  return(
-    <div className="todo-list">
-      <p style={style}>Main component</p>
-      {data}
-    </div>
-  );
+
+  render() {
+    todosItem = this.state.data.map(todo => <TodoItem todoItem={todo} key={todo.id} />)
+
+    const style = {
+      fontSize: 30,
+      color: "#FF8C00"
+    }
+    return (
+      <div className="todo-list">
+        <p style={style}>Main component</p>
+        {todosItem}
+      </div>
+    );
+  }
 }
 
 export default Main;
